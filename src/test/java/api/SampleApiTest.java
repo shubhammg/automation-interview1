eba518329fe91f85a4898a4b0a065fa534cf6c5b
shubhammg <tandonshubham70@gmail.com>
2025-06-22 11:07:11 +0530

    changes done

--- src/test/java/api/SampleApiTest.java
+++ src/test/java/api/SampleApiTest.java
@@ -1,14 +1,65 @@
 package api;
 
-import io.restassured.response.Response;
-import org.testng.Assert;
+import io.restassured.http.ContentType;
+//import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
-public class SampleApiTest extends ApiBase {
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class SampleApiTest {
+
     @Test
-    public void getUserTest() {
-        Response response = getRequest()
-                .get("/api/users/2");
-        Assert.assertEquals(response.getStatusCode(), 200);
+    public void testGetUser() {
+        given()
+            .baseUri("https://reqres.in")
+        .when()
+            .get("/api/users/2")
+        .then()
+            .statusCode(200)
+            .body("data.id", equalTo(2))
+            .body("data.email", containsString("@reqres.in"));
+    }
+
+    @Test
+    public void testPostUser() {
+        String requestBody = "{ \"name\": \"Shubham\", \"job\": \"QA\" }";
+
+        given()
+            .baseUri("https://reqres.in")
+            .contentType(ContentType.JSON)
+            .body(requestBody)
+        .when()
+            .post("/api/users")
+        .then()
+            .statusCode(201)
+            .body("name", equalTo("Shubham"))
+            .body("job", equalTo("QA"));
+    }
+
+    @Test
+    public void testPutUser() {
+        String requestBody = "{ \"name\": \"Shubham\", \"job\": \"Lead QA\" }";
+
+        given()
+            .baseUri("https://reqres.in")
+            .contentType(ContentType.JSON)
+            .body(requestBody)
+        .when()
+            .put("/api/users/2")
+        .then()
+            .statusCode(200)
+            .body("name", equalTo("Shubham"))
+            .body("job", equalTo("Lead QA"));
+    }
+
+    @Test(enabled=false)
+    public void testDeleteUser() {
+        given()
+            .baseUri("https://reqres.in")
+        .when()
+            .delete("/api/users/2")
+        .then()
+            .statusCode(204);
     }
 }
